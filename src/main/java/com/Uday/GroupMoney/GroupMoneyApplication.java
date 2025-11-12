da5c99450eb44b471e78612c126f6ff72413cbe0
Dharmendra1801 <dharmendramishra1815@gmail.com>
2025-11-12 08:23:53 +0530

    Initialised the project

---
+++ src/main/java/com/Uday/GroupMoney/GroupMoneyApplication.java
@@ -0,0 +1,13 @@
+package com.Uday.GroupMoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Uday/GroupMoney/GroupMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Uday.GroupMoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroupMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
